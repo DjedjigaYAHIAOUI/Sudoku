bf2c06d010cb5d1f06e6a7e0d23435099b6d3d2e
hafsati marwa <marwa.hafsati@etu.u-pec.fr>
2024-04-08 23:34:16 +0200

    grille

---
+++ Grille.java
@@ -0,0 +1,14 @@
+public class Grille{
+    public static void main(String args){
+
+	private int[][]Grille;
+	private int[][]Solution;
+
+
+	public Grille{ 
+	    Grille=new int[9][9];
+	    Solution= new int [9][9];
+	}
+
+	
+	
